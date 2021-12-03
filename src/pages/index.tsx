ba79036ced487e46058b51b6ea312fa265532cf5
svrakata <svrakata@gmail.com>
2021-12-03 10:11:27 +0200

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,24 +58,12 @@ const infographData: IInfograph = {
 const metaData: PageMetaData = {
     image: [
         {
-            src: `${BASE_URL}/infograph_1.jpg`,
-            type: "image/jpeg",
+            src: `${BASE_URL}/infograph_1.png`,
+            type: "image/png",
         },
         {
-            src: `${BASE_URL}/infograph_2.jpg`,
-            type: "image/jpeg",
-        },
-        {
-            src: `${BASE_URL}/infograph_3.jpg`,
-            type: "image/jpeg",
-        },
-        {
-            src: `${BASE_URL}/infograph_4.jpg`,
-            type: "image/jpeg",
-        },
-        {
-            src: `${BASE_URL}/infograph_5.jpg`,
-            type: "image/jpeg",
+            src: `${BASE_URL}/infograph_2.png`,
+            type: "image/png",
         },
     ],
 };
