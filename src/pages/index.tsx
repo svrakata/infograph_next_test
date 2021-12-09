1a1d019316aba2adc6642a7fc14be99582f8665f
svrakata <svrakata@gmail.com>
2021-12-09 08:48:04 +0200

    joke

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,11 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/layout";
 import { Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { PageMetaData } from "../components/Head";
-import Infograph, { IInfograph } from "../components/infograph/Infograph";
 
 import Layout from "../components/Layout";
 import SvgInfograph from "../components/svg/SvgInfograph";
 import { BASE_URL } from "../constants/pageMetaData";
-
-const IMAGE_SRC =
-    "https://www.pngkit.com/png/full/136-1364547_turmeric-powder-prozac-natural-equivalent.png";
-
-const infographData: IInfograph = {
-    image: IMAGE_SRC,
-    items: [
-        {
-            icon: "kkk",
-            title: "Helps to reduce inflammation",
-        },
-        {
-            icon: "kkk",
-            title: "Helps to relieve arthritic pain",
-        },
-        {
-            icon: "kkk",
-            title: "Helps to heal stomach ulcers",
-        },
-        {
-            icon: "kkk",
-            title: "Helps to enhance respiratory health",
-        },
-        {
-            icon: "kkk",
-            title: "Helps to strengthen ligaments",
-        },
-        {
-            icon: "kkk",
-            title: "Is great for the skin",
-        },
-        {
-            icon: "kkk",
-            title: "Helps wounds heal faster",
-        },
-        // {
-        //     icon: "kkk",
-        //     title: "Helps improve brain function",
-        // },
-        // {
-        //     icon: "kkk",
-        //     title: "Reduces cholesterol",
-        // },
-        // {
-        //     icon: "kkk",
-        //     title: "Helps combat depression",
-        // },
-    ],
-};
 
 const metaData: PageMetaData = {
     image: [
